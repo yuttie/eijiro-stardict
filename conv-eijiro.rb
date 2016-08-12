8f82c2fd0f72beb1180220e69355fd8853f7a8bb
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2016-08-12 18:44:42 +0900

    Read lines with ARGF

--- conv-eijiro.rb
+++ conv-eijiro.rb
@@ -1,67 +1,67 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
+ARGF.set_encoding('CP932:UTF-8')
+
 current = nil
-File.open(ARGV[0], 'r:CP932:UTF-8') do |file|
-  file.each_line do |line|
-    line.chomp!
-    line.gsub!('\\', '¥')
-    line.gsub!('<', '&lt;')
-    line.gsub!('>', '&gt;')
-    line.gsub!('〔', '（')
-    line.gsub!('〕', '）')
-    item, desc = line.split(' : ', 2)
-    next if desc == ''
-    item =~ /^■(.+?)(?: +\{([^{}]+)\})?$/
-    entry = $1
-    pos = $2
-    desc2, *examples = desc.split('■・')
-    examples.map! do |ex|
-      ex.split('◆')
-    end
-    desc3, note = desc2.split('◆')
-    desc3.gsub!(/｛.+?｝/, '')
-    note.gsub!(/｛.+?｝/, '') if note
+ARGF.each_line do |line|
+  line.chomp!
+  line.gsub!('\\', '¥')
+  line.gsub!('<', '&lt;')
+  line.gsub!('>', '&gt;')
+  line.gsub!('〔', '（')
+  line.gsub!('〕', '）')
+  item, desc = line.split(' : ', 2)
+  next if desc == ''
+  item =~ /^■(.+?)(?: +\{([^{}]+)\})?$/
+  entry = $1
+  pos = $2
+  desc2, *examples = desc.split('■・')
+  examples.map! do |ex|
+    ex.split('◆')
+  end
+  desc3, note = desc2.split('◆')
+  desc3.gsub!(/｛.+?｝/, '')
+  note.gsub!(/｛.+?｝/, '') if note
 
-    content = []
-    content << desc3
-    content << "<span size=\"x-small\">※#{note}</span>" if note
-    examples.each do |ex, ex_note|
-      content << "<span size=\"small\">• #{ex}</span>"
-      content << "<span size=\"small\"><span color=\"#FFFFFF00\">• </span>#{ex_note}</span>" if ex_note
-    end
+  content = []
+  content << desc3
+  content << "<span size=\"x-small\">※#{note}</span>" if note
+  examples.each do |ex, ex_note|
+    content << "<span size=\"small\">• #{ex}</span>"
+    content << "<span size=\"small\"><span color=\"#FFFFFF00\">• </span>#{ex_note}</span>" if ex_note
+  end
 
-    if !current
-      # replace
-      current = {
-        entry: entry,
-        contents: [[pos, content]]
-      }
-    elsif current[:entry] == entry
-      # append
-      current[:contents] << [pos, content]
-    else
-      # output
-      print("#{current[:entry]}\t")
-      lines = []
-      current[:contents].each do |pos, content|
-        if pos
-          lines << "<span weight=\"bold\">〔#{pos}〕</span>#{content[0]}" if pos
-          content[1..-1].each do |c|
-            lines << "\t#{c}"
-          end
-        else
-          content.each do |c|
-            lines << "#{c}"
-          end
+  if !current
+    # replace
+    current = {
+      entry: entry,
+      contents: [[pos, content]]
+    }
+  elsif current[:entry] == entry
+    # append
+    current[:contents] << [pos, content]
+  else
+    # output
+    print("#{current[:entry]}\t")
+    lines = []
+    current[:contents].each do |pos, content|
+      if pos
+        lines << "<span weight=\"bold\">〔#{pos}〕</span>#{content[0]}" if pos
+        content[1..-1].each do |c|
+          lines << "\t#{c}"
+        end
+      else
+        content.each do |c|
+          lines << "#{c}"
         end
       end
-      puts(lines.join('\n'))
-      # replace
-      current = {
-        entry: entry,
-        contents: [[pos, content]]
-      }
     end
+    puts(lines.join('\n'))
+    # replace
+    current = {
+      entry: entry,
+      contents: [[pos, content]]
+    }
   end
 end
