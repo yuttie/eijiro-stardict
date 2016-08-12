34d89db22da91ceac0a2759c377c4aab309d7d11
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2016-08-12 14:16:03 +0900

    Improve the appearance

--- conv-eijiro.rb
+++ conv-eijiro.rb
@@ -7,6 +7,11 @@ current = nil
 File.open('EIJIRO/EIJI-1441.TXT', 'r:CP932:UTF-8') do |file|
   file.each_line do |line|
     line.chomp!
+    line.gsub!('\\', '¥')
+    line.gsub!('<', '&lt;')
+    line.gsub!('>', '&gt;')
+    line.gsub!('〔', '（')
+    line.gsub!('〕', '）')
     item, desc = line.split(' : ', 2)
     _, entry, _, pos = /^■(.+?)( +\{([^{}]+)\})?$/.match(item).to_a
     next if desc == ''
@@ -22,8 +27,8 @@ File.open('EIJIRO/EIJI-1441.TXT', 'r:CP932:UTF-8') do |file|
     content << desc3
     content << "<span size=\"x-small\">※#{note}</span>" if note
     examples.each do |ex, ex_note|
-      content << "<span size=\"small\">・#{ex}</span>"
-      content << "<span size=\"small\">#{ex_note}</span>" if ex_note
+      content << "<span size=\"small\">• #{ex}</span>"
+      content << "<span size=\"small\"><span color=\"#FFFFFF00\">• </span>#{ex_note}</span>" if ex_note
     end
 
     if !current
@@ -40,9 +45,15 @@ File.open('EIJIRO/EIJI-1441.TXT', 'r:CP932:UTF-8') do |file|
       print("#{current[:entry]}\t")
       lines = []
       current[:contents].each do |pos, content| 
-        lines << "<b>〔#{pos}〕</b>" if pos
-        content.each do |c| 
-          lines << "    #{c}"
+        if pos
+          lines << "<span weight=\"bold\">〔#{pos}〕</span>#{content[0]}" if pos
+          content[1..-1].each do |c|
+            lines << "\t#{c}"
+          end
+        else
+          content.each do |c|
+            lines << "#{c}"
+          end
         end
       end
       puts(lines.join('\n'))
